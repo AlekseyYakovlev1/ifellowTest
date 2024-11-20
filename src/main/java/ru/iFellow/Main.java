4d053b0fec7e5c80a54832827d406cbb6249e5e7
Яковлев алексей <wwaannhheeddaa@gmail.com>
2024-11-20 16:06:04 +0400

    commit

--- src/main/java/ru/iFellow/Main.java
+++ src/main/java/ru/iFellow/Main.java
@@ -6,18 +6,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static <Car> void main(String[] args) {
+    public static void main(String[] args) {
         List<Car> carList = new ArrayList<Car>();
-        carList.add((Car) new Suzuki(2010, "Автомат", "Зеленый", "Бензин", "SUZ123456789"));
-        carList.add((Car) new Toyota(2005, "Механика", "Красный", "Дизель", "TOY123456789"));
-        carList.add((Car) new Ford(2018, "Автомат", "Синий", "Бензин", "FOR123456789"));
-        carList.add((Car) new Honda(2002, "Механика", "Зеленый", "Электро", "HON123456789"));
-        carList.add((Car) new BMW(2020, "Автомат", "Черный", "Бензин", "BMW123456789"));
-        carList.add((Car) new Suzuki(2001, "Механика", "Синий", "Дизель", "SUZ987654321"));
-        carList.add((Car) new Toyota(2015, "Автомат", "Зеленый", "Бензин", "TOY987654321"));
-        carList.add((Car) new Ford(2008, "Механика", "Красный", "Дизель", "FOR987654321"));
-        carList.add((Car) new Honda(2021, "Автомат", "Черный", "Электро", "HON987654321"));
-        carList.add((Car) new BMW(2007, "Механика", "Зеленый", "Бензин", "BMW987654321"));
+        carList.add( new Suzuki(2010, "Автомат", "Зеленый", "Бензин", "SUZ123456789"));
+        carList.add( new Toyota(2005, "Механика", "Красный", "Дизель", "TOY123456789"));
+        carList.add( new Ford(2018, "Автомат", "Синий", "Бензин", "FOR123456789"));
+        carList.add( new Honda(2002, "Механика", "Зеленый", "Электро", "HON123456789"));
+        carList.add( new BMW(2020, "Автомат", "Черный", "Бензин", "BMW123456789"));
+        carList.add( new Suzuki(2001, "Механика", "Синий", "Дизель", "SUZ987654321"));
+        carList.add( new Toyota(2015, "Автомат", "Зеленый", "Бензин", "TOY987654321"));
+        carList.add( new Ford(2008, "Механика", "Красный", "Дизель", "FOR987654321"));
+        carList.add( new Honda(2021, "Автомат", "Черный", "Электро", "HON987654321"));
+        carList.add( new BMW(2007, "Механика", "Зеленый", "Бензин", "BMW987654321"));
         printCarsAfter2006(carList);
         changeGreenToRed(carList);
         printCarsAfter2006(carList);
--- src/main/java/ru/iFellow/classes/Car.java
+++ src/main/java/ru/iFellow/classes/Car.java
@@ -1,7 +1,5 @@
 package ru.iFellow.classes;
 
-import java.util.ArrayList;
-import java.util.List;
 public abstract class Car {
     String model;
     int yearOfManufacture;
